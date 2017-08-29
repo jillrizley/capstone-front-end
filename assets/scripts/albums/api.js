51daed94190b4f0d5df83f3f0a37a6bf4c7d37bc
jillrizley <jillian.rizley@gmail.com>
2017-08-28 22:56:55 -0400

    add api for albums

--- assets/scripts/albums/api.js
+++ assets/scripts/albums/api.js
@@ -1,19 +1,59 @@
 'use strict'
-
-const config = require('../config')
 const store = require('../store')
+const config = require('../config.js')
 
-
-const addAlbum = () => {
+const add = function (data) {
+  console.log('hello')
   return $.ajax({
-    url: config.apiOrigin + 'albums',
     method: 'POST',
+    url: config.apiOrigin + '/albums',
+    headers: {
+      contentType: 'application/json',
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+const destroy = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/albums/' + data,
+    method: 'DELETE',
     headers: {
       Authorization: 'Token token=' + store.user.token
     }
   })
 }
 
+const show = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/albums/' + data,
+    method: 'GET'
+  })
+}
+
+const update = function (data, id) {
+  return $.ajax({
+    method: 'PATCH',
+    url: config.apiOrigin + '/albums/' + id,
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+const index = function () {
+  return $.ajax({
+    url: config.apiOrigin + '/albums',
+    method: 'GET'
+  })
+}
+
 module.exports = {
-  addAlbum
+  add,
+  destroy,
+  show,
+  update,
+  index
 }
