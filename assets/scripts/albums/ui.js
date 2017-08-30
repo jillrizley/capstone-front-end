1c2ce4b89156aadc64c27d299e4c7e3a4d35d9cf
jillrizley <jillian.rizley@gmail.com>
2017-08-30 12:41:38 -0400

    add photo form text in black instead of white

--- assets/scripts/albums/ui.js
+++ assets/scripts/albums/ui.js
@@ -5,15 +5,12 @@ const albumTemplate = require('../templates/album.hbs')
 const onAddSuccess = function (data) {
   $('#createAlbumModal').modal('hide')
   $('#content').append(albumTemplate(data.album))
-  $('#successNotify').css('display', 'block').text('Your album is created.')
-  $('#errorNotify').css('display', 'none')
+  $('#createAlbumForm')[0].reset()
 }
 const onAddError = function (error) {
   console.log(error)
   $('#createAlbumModal').modal('hide')
   $('#content').empty()
-  $('#errorNotify').css('display', 'block').text('There was a problem creating your album.')
-  $('#successNotify').css('display', 'none')
 }
 
 const onShowSuccess = function (data) {
@@ -26,6 +23,7 @@ const onShowSuccess = function (data) {
 const onUpdateSuccess = function (data) {
   $('#editAlbumModal').modal('hide')
   $('#content').append(albumTemplate(data.album))
+  $('#editAlbumForm')[0].reset()
 }
 
 const onDestroySuccess = function () {
